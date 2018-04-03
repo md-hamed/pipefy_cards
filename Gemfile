93555d9b95d17da8b7319b4a6f1cf469ce6bfd8b
md-hamed <mohamed.hamed@espace.com.eg>
2018-04-03 16:29:20 +0200

    Install/configure shoulda-matchers gem

--- Gemfile
+++ Gemfile
@@ -54,6 +54,7 @@ end
 
 group :test do
   gem 'database_cleaner', '~> 1.6.2'
+  gem 'shoulda-matchers', '~> 3.1'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -27,6 +27,14 @@ require 'database_cleaner'
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
+# configure shoulda matchers
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
 RSpec.configure do |config|
   # include Factory Bot syntax
   config.include FactoryBot::Syntax::Methods
