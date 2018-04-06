3c5d5d34fe122c7c61b21abeb1406c25be18270d
md-hamed <mohamed.hamed@espace.com.eg>
2018-04-06 16:37:56 +0200

    Add basic Pipe validations

--- app/models/pipe.rb
+++ app/models/pipe.rb
@@ -1,3 +1,8 @@
 class Pipe < ApplicationRecord
+  # validations
+  validates :name, presence: true
+  validates :pipefy_id, presence: true, uniqueness: true
+
+  # associations
   belongs_to :organization
 end
