8edb89d306f65cb5d81ad1a540be0de2d7a1392f
md-hamed <mohamed.hamed@espace.com.eg>
2018-04-08 03:39:05 +0200

    Fix stubbed request headers

--- spec/support/stupped_requests.rb
+++ spec/support/stupped_requests.rb
@@ -4,9 +4,7 @@ module StubbedRequests
     stub_request(:post, "https://app.pipefy.com/queries").with(
       headers: {
       'Accept'=>'*/*',
-      'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
       'Content-Type'=>'application/json',
-      'User-Agent'=>'Faraday v0.14.0'
        }).to_return(status: 200, body: body, headers: {})
   end
 end
