cbf090ff050c6c0706abaf7bed20be044ed14a43
md-hamed <mohamed.hamed@espace.com.eg>
2018-04-08 02:29:06 +0200

    Refactor flash errors on organization controller

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,9 +1,11 @@
 class OrganizationsController < ApplicationController
   def import
     if Pipefy::SyncOrganizationGraph.new(organization_id: 92858, pipe_id: 335557).perform
-      redirect_to root_path, flash: { notice: 'Organization imported successfully!' }
+      flash[:notice] = 'Organization imported successfully!'
     else
-      redirect_to root_path, flash: { error: 'Organization was not imported due to an error.' }
+      flash[:error] = 'Organization was not imported due to an error.'
     end
+    
+    redirect_to root_path
   end
 end
