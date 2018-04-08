d116eb61840308b377c8d39ce49f54d92e154309
md-hamed <mohamed.hamed@espace.com.eg>
2018-04-08 02:05:52 +0200

    Add test organization/pipe ids

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,6 +1,6 @@
 class OrganizationsController < ApplicationController
   def import
-    if Pipefy::SyncOrganizationGraph.new(organization_id: 114095, pipe_id: 423922).perform
+    if Pipefy::SyncOrganizationGraph.new(organization_id: 92858, pipe_id: 335557).perform
       redirect_to root_path, flash: { notice: 'Organization imported successfully!' }
     else
       redirect_to root_path, flash: { error: 'Organization was not imported due to an error.' }
