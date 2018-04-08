b70ec65589c4f2937d3f2a3058da7ee578836e2c
md-hamed <mohamed.hamed@espace.com.eg>
2018-04-08 02:14:31 +0200

    Add missing phase association spec

--- spec/models/phase_spec.rb
+++ spec/models/phase_spec.rb
@@ -6,4 +6,5 @@ RSpec.describe Phase, type: :model do
   it { is_expected.to validate_uniqueness_of :pipefy_id }
 
   it { is_expected.to belong_to :pipe }
+  it { is_expected.to have_many :cards }
 end
